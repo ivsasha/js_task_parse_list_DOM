843df9203eeb6d06404a7ecf6a5bc9337f2ec344
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-21 02:42:22 +0300

    task done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-// write code here
+const li = Array.from(document.querySelectorAll('li'));
+
+sortList(li);
+getEmployees(li);
+
+function sortList(list) {
+  const ul = document.querySelector('ul');
+
+  const sortedItems = list.sort((a, b) => {
+    const salaryA = parseInt(a.getAttribute('data-salary').slice(1));
+    const salaryB = parseInt(b.getAttribute('data-salary').slice(1));
+
+    return salaryA - salaryB;
+  });
+
+  ul.innerHTML = '';
+  sortedItems.forEach((item) => ul.appendChild(item));
+}
+
+function getEmployees(list) {
+  const employees = [];
+
+  list.forEach((item) => {
+    const names = item.textContent.trim();
+    const salary = item.getAttribute('data-salary');
+    const position = item.getAttribute('data-position');
+    const age = item.getAttribute('data-age');
+
+    employees.push({
+      name: names,
+      salary: salary,
+      position: position,
+      age: age,
+    });
+  });
+
+  return employees;
+}
